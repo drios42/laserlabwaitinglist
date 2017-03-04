03d651340f8fc9f929bd9454c6731ac71d00b466
drios42 <drios42@gmail.com>
2017-03-03 19:00:12 -0500

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/test');
+var Schema = mongoose.Schema;
+
+// twilio
+var accountSid = 'AC92e3e8130e57849cdcbe4179eaec0d5b';
+var authToken = 'b55eef88acf6c5f2443f39dc2314711d';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
+
+
+router.post('/notify', function(req, res, next){
+  var phonenum = req.body.phone;
+  var nombre = req.body.name;
+  console.log(phonenum);
+
+  client.messages.create({
+      to: phonenum,
+      from: "+13479527379",
+      body: "Hi "+ nombre+", are you ready to LAZZZZZZZZZER?!?!?!?!",
+  }, function(err, message) {
+    if(err){
+      console.log(err)
+    }else{
+      console.log(message.body);
+      console.log(message.sid);
+    }
+  });
+});
+
+
+var userDataSchema = new Schema({
+  name: {type: String, required: true},
+  phone: String,
+  email: String
+}, {collection: 'user-data'});
+
+var UserData = mongoose.model('UserData', userDataSchema);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/get-data', function(req, res, next) {
+  UserData.find()
+      .then(function(doc) {
+        res.render('index', {items: doc});
+      });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email
+  };
+
+  var data = new UserData(item);
+  data.save();
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var id = req.body.id;
+
+  UserData.findById(id, function(err, doc) {
+    if (err) {
+      console.error('error, no entry found');
+    }
+    doc.name = req.body.name;
+    doc.phone = req.body.phone;
+    doc.email= req.body.email;
+    doc.save();
+  })
+  res.redirect('/');
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+  UserData.findByIdAndRemove(id).exec();
+  res.redirect('/');
+});
+
+module.exports = router;
